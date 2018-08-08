4771c53be969799c295e773204cefab40c681cf4
Kira~ <MiracleUa@hotmail.com>
2018-08-08 16:09:05 +0300

    added file read

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,5 +1,7 @@
 import calculator.Calculator;
 import calculator.impl.CalculImpl;
+import reader.MatrixReader;
+import reader.impl.MatrixReaderImpl;
 
 import java.io.*;
 
@@ -10,35 +12,36 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
-        System.out.println("First array: ");
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-        }
-
-        System.out.println("  ");
-
-        System.out.println("Second array: ");
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                System.out.print(b[i][j] + " ");
-            }
-        }
-
-        System.out.println("          ");
-        System.out.println("*-*-*Answer*-*-*");
-
-        Calculator calc = new CalculImpl();
-
-        int [][] result = calc.multiplicat(a,b);
-
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result.length; j++) {
-                System.out.print(result[i][j] + " ");
-            }
-        }
+        MatrixReader matrixReader = new MatrixReaderImpl();
+        matrixReader.readMatrixData("Example.txt");
+//         System.out.println("First array: ");
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = 0; j < a.length; j++) {
+//                System.out.print(a[i][j] + " ");
+//            }
+//        }
+//
+//        System.out.println("  ");
+//
+//        System.out.println("Second array: ");
+//        for (int i = 0; i < b.length; i++) {
+//            for (int j = 0; j < b.length; j++) {
+//                System.out.print(b[i][j] + " ");
+//            }
+//        }
+//
+//        System.out.println("          ");
+//        System.out.println("*-*-*Answer*-*-*");
+//
+//        Calculator calc = new CalculImpl();
+//
+//        int [][] result = calc.multiplicat(a,b);
+//
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < result.length; j++) {
+//                System.out.print(result[i][j] + " ");
+//            }
+//        }
 
     }
 
--- src/java/reader/impl/MatrixReaderImpl.java
+++ src/java/reader/impl/MatrixReaderImpl.java
@@ -6,6 +6,7 @@ public class MatrixReaderImpl implements MatrixReader {
 
     @Override
     public int[][] readMatrixData(String fileName) {
+        System.out.println(this.getClass().getClassLoader().getResource(fileName));
         return new int[0][];
     }
 }
